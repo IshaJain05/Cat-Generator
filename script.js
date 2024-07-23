4d371921ace4565db9247896bc7a662e181c7d07
isha <67365987+IshaJain05@users.noreply.github.com>
2024-07-23 17:35:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    //image.src = "http://thecatapi.com/api/images/get?format=src&type=gif" ;
+    image.src = "https://media.tenor.com/kQA86PqyXZQAAAAj/small-dancing-white-cat-dance-funny.gif";
+    div.appendChild(image);
+}
